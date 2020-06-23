aff23ea6946fc9ad27270650e2943727159bd198
Cristiano <cristianomorgante@hotmail.com>
2020-06-22 21:57:42 -0300

    Add Global Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <h1>GoBarber</h1>
+    <>
+      <GlobalStyle />
+      <h1>GoBarber</h1>
+    </>
   );
 }
 
