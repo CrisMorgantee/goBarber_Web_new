86ecf62418d038906d141ee8b60f1a59d32c3035
Cristiano <cristianomorgante@hotmail.com>
2020-06-28 16:54:08 -0300

    Include unform, Add validation and Tooltip on SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,42 +1,72 @@
-import React from 'react';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import React, { useCallback, useRef } from 'react';
 import {
   FiLock as IconLock,
   FiLogIn as IconLogin,
   FiMail as IconMail,
 } from 'react-icons/fi';
+import * as Yup from 'yup';
 import Logo from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
+import getValidationErrors from '../../utils/getValidationErrors';
 import * as S from './styled';
 
-const SignIn: React.FC = () => (
-  <S.Container>
-    <S.Content>
-      <img src={Logo} alt="Logo GoBarber" />
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-      <form>
-        <h1>Faça seu logon</h1>
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
 
-        <Input name="email" icon={IconMail} placeholder="E-mail" type="email" />
-        <Input
-          name="password"
-          icon={IconLock}
-          placeholder="Senha"
-          type="password"
-        />
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email('Esse não é um email válido')
+          .required('Email obrigatório'),
+        password: Yup.string().required(
+          'A senha é obrigatória, mínimo de 6 dígitos',
+        ),
+      });
 
-        <Button type="submit">Entrar</Button>
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      const errors = getValidationErrors(error);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
+  return (
+    <S.Container>
+      <S.Content>
+        <img src={Logo} alt="Logo GoBarber" />
 
-      <a href="login">
-        <IconLogin />
-        Criar conta
-      </a>
-    </S.Content>
-    <S.Background />
-  </S.Container>
-);
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+
+          <Input name="email" icon={IconMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={IconLock}
+            placeholder="Senha"
+            type="password"
+          />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a href="login">
+          <IconLogin />
+          Criar conta
+        </a>
+      </S.Content>
+      <S.Background />
+    </S.Container>
+  );
+};
 
 export default SignIn;
